93a7fb5a6b042ff841cf725985040e8920f6b65d
inkhl369 <60577224+inkhl369@users.noreply.github.com>
2020-03-02 23:40:32 +0300

    init: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+
+string = String(n);
+var i, a = [];
+var arr = string.split('');
+    for (i = 0; i < arr.length; i++) {
+        if(arr[i] >= 0) {
+        a.push(arr[i]);
+        }
+    }
+
+    return a.reverse().join('');
 }
